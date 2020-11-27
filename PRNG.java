eec6f0877293708f9b1693f3e62c3dc1dd1830a0
Pranav Teegavarapu <pranavnt@outlook.com>
2020-11-26 17:07:56 -0800

    started work

---
+++ PRNG.java
@@ -0,0 +1,18 @@
+public class PRNG {
+    public final int SEED = 4321;
+    public final int GENERATOR = 223;
+    public final int MODULUS = 4321;
+
+    public static void main(String[] args) {
+
+    }
+
+    public static double prng() {
+
+    }
+
+    public static double l(double x) {
+        return x * x - 2 * x + 1;
+    }
+
+}
